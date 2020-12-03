40b373cfc71c36dea094cfd9a7de00f4fe9d6d1f
Leonel Velez <leonel-14-velez@hotmail.com>
2020-12-03 00:18:43 -0500

    hide and show nav bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,36 +7,67 @@ export default class Header extends Component {
  constructor(props) {
     super(props);
     this.menu =  Vars.Menu;
-    
+    this.state = {
+      Menu: {
+        gradosMenu : 'rotateY(90)',
+        open: false
+      }
+    }
+    console.log(this.state)
   }
 
   render() {
 
     return (
       <div>          
-          <header>
-              <li >
-                <img id="logoHeader"  src={LogoHeader} alt="logo of Leonel Velez" >
-                </img>
-              </li>  
-              <li>
-                <div id="logoMenu">
+          <header>              
+              <img id="logoHeader"  src={LogoHeader} alt="logo of Leonel Velez" ></img>              
+              <section id="navBar" className="container row">
+                <ul id="nav" className="container row"  style={{transform: this.state.Menu.gradosMenu}}>                
+                  {
+                    this.menu.map((obje)=>{
+                    return <li key={obje.id}>{obje.name}</li>
+                    })                                                              
+                  }                                          
+                </ul>
+                <div id="logoMenu" onClick={this.onNav}>
                   <p></p>
                   <p></p>
                 </div>
-                 
-              </li>  
-          </header>        
-          <ul id="nav" >                
-              {
-                this.menu.map((obje)=>{
-                return <li key={obje.id}>{obje.name}</li>
-                })                                                              
-              }                                          
-          </ul>
+              </section>                            
+          </header>                  
       </div>              
     );
   }
+
+  onNav = ()=>{    
+    if (!this.state.Menu.open)
+    {
+      this.OpenMenu()  
+    }else{
+       this.CloseMenu()     
+    }  
+
+  }
+
+  OpenMenu = ()=>{
+    this.setState({
+      Menu : {
+        gradosMenu: 'rotateY(0deg)',
+        open : true
+      }        
+    })      
+  }
+
+  CloseMenu = ()=>{
+    this.setState({
+      Menu : {
+        gradosMenu: 'rotateY(90deg)',
+          open : false
+        }        
+      })      
+    }
+  
 }
  
  
