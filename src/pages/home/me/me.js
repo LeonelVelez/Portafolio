b87785f2b64c499645d9069d900a01e1acc01000
Leonel Velez <leonel-14-velez@hotmail.com>
2021-01-05 16:14:02 -0500

    image about me

--- src/pages/home/me/me.js
+++ src/pages/home/me/me.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import ContainerViewPort from '../../../components/containerviewPort/containerviewPort';
-
+import  './me.css';
 export default class Me extends Component
 {
     constructor(props)
@@ -15,24 +15,8 @@ export default class Me extends Component
         return (
             <>
                 <section id="me" className="container row" ref={this.me}>
-                    <div></div>
-                    <div id="imagenIlustra"></div>
-                    <div id="description">
-                        <h2> Who i'm? How can i help u? </h2>
-                        <p> 
-                            Well! I'm software engineer, I'm 22 years old and I have worked on many software proyects in Guayaquil, Ecuador.
-                            As you can see, I can speak both languages (english and spanish), and this is not the only skill I have. Next,  you can check out my skills and Portafolio.
-                            <br></br>
-                            <br></br>
-                            <span id="cita">
-                                "La ignorancia es la cuna del miedo pero a mi no me da miedo preguntar por qué?"
-                                <span id="autor" ></span>
-                            </span>
-                            <br></br>
-                            <button>Checkout my Skills</button>
-                            <button>Or maybe my Porfafolio</button>
-                        </p>
-                    </div>
+                    <div className="child"></div>
+                    <div className="child" id="imagenIlustra"></div>                                                    
                     <ContainerViewPort type={'me'} references={this.me} documentInfo={this.props.documentInfo} onFocusViewPort={this.onFocusViewPort} ></ContainerViewPort>
                 </section>
             </>
