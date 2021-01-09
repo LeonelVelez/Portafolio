e12df25a9f12a90314dac87667e6b507f5adb9c3
Leonel Velez <leonel-14-velez@hotmail.com>
2021-01-08 20:30:10 -0500

    adding capa class

--- src/pages/home/me/me.js
+++ src/pages/home/me/me.js
@@ -18,25 +18,28 @@ export default class Me extends Component
                 <section id="me" className="container row" ref={this.me}>
                     <div className="child"></div>
                     <div className="child" id="imagenIlustra"></div>    
-                    <div id="description">
-                        <h2> Who i'm? How can i help u? </h2>
-                        <p> 
-                            Well! I'm software engineer, I'm 22 years old and I have worked on many software proyects in Guayaquil, Ecuador.
-                            As you can see, I can speak both languages (english and spanish), and this is not the only skill I have. Next,  you can check out my skills and Portafolio.
-                            <br></br>
-                            <br></br>
-                            <span id="cita">
-                                "La ignorancia es la cuna del miedo pero a mi no me da miedo preguntar por qué?"
+                    <div id="description" className="container center">
+                        <section >
+                            <h2> Who i'm? How can i help u? </h2>
+                            <p> 
+                                Well! I'm software engineer, I'm 22 years old and I have worked on many software proyects in Guayaquil, Ecuador.
+                                As you can see, I can speak both languages (english and spanish), and this is not the only skill I have. Next,  you can check out my skills and Portafolio.
                                 <br></br>
-                                <span id="autor" >- Roberto Musso</span>
-                            </span>
-                            <br></br>         
-                            <div className="container row center" id="buttons">
-                                <button className="button offline ">Checkout my Skills</button>
-                                <button className="button outline ">Or maybe my Porfafolio</button>                                                        
-                            </div>                   
-                            
-                        </p>
+                                <br></br>
+                                <span id="cita">
+                                    "La ignorancia es la cuna del miedo pero a mi no me da miedo preguntar por qué?"
+                                    <br></br>
+                                    <span id="autor" >- Roberto Musso</span>
+                                </span>
+                                <br></br>         
+                                <div className="container row center" id="buttons">
+                                    <button className="button offline ">Checkout my Skills</button>
+                                    <button className="button outline ">Or maybe my Porfafolio</button>                                                        
+                                </div>                   
+                                
+                            </p>
+                        </section>
+                        
                     </div>                                                
                     <ContainerViewPort type={'me'} references={this.me} documentInfo={this.props.documentInfo} onFocusViewPort={this.onFocusViewPort} ></ContainerViewPort>
                 </section>
