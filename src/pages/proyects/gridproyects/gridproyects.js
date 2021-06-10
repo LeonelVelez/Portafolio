bc122a86c4e1dfc5c1533ffb653836f4a7f0bcb3
Leonel Velez <leonel-14-velez@hotmail.com>
2021-06-10 15:42:42 -0500

    responisve design to grid proyect component

--- src/pages/proyects/gridproyects/gridproyects.js
+++ src/pages/proyects/gridproyects/gridproyects.js
@@ -11,8 +11,28 @@ export default class GridProyects extends Component
         this.props.children.forEach(re=>{
             console.log(re);
         })
-        this.lengthRows = 0;
-        this.addRows();
+        
+        if(window.matchMedia("(min-width: 1200px)").matches)
+        {
+            this.addRows(0);
+        }
+        if(window.matchMedia("(max-width: 1200px)").matches)
+        {
+            this.addRows(1);
+        }
+        window.matchMedia("(min-width: 1200px)").addEventListener('change', (x)=>{
+            if(x.matches)
+            {
+                this.addRows(0);
+            }            
+        })
+        window.matchMedia("(max-width: 1200px)").addEventListener('change', (x)=>{
+            if(x.matches)
+            {
+                this.addRows(1);
+            }            
+        })
+        
     }
     render()
     {
@@ -40,8 +60,16 @@ export default class GridProyects extends Component
 
         var gridRowFromItem4 = 0;
         var gridRowToItem4 = 0;
-        const mult = 16 + 1;
+        let mult = 0;
+        if(window.matchMedia("(max-width: 1200px)").matches)
+        {
+            mult = 24;
+        }else{
+            mult = 16 + 1;
+        }
+        
         return this.props.children.map(children => {
+            
             iterator++;
             if(iterator === 5)
             {
@@ -55,12 +83,25 @@ export default class GridProyects extends Component
                 case 'item-1':
                     if(iteratorGrid === 1)
                     {
-                        gridRowFromItem1 = 1;
-                        gridRowToItem1 = 7;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowFromItem1 = 1;
+                            gridRowToItem1 = 6;
+                        }else{
+                            gridRowFromItem1 = 1;
+                            gridRowToItem1 = 7;
+                        }
+                        
                     }else{
                         
-                        gridRowFromItem1 = gridRowFromItem1+ mult;                    
-                        gridRowToItem1 = gridRowToItem1 + mult;
+                        gridRowFromItem1 = gridRowFromItem1+ mult;          
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowToItem1 = gridRowFromItem1 + +5;
+                        }else{
+                            gridRowToItem1 = gridRowToItem1 + mult;
+                        }          
+                        
                     }
                     
                     return React.cloneElement(children, {style:{gridRow: `${gridRowFromItem1}/${gridRowToItem1}`}});
@@ -68,11 +109,23 @@ export default class GridProyects extends Component
                 case 'item-2':
                     if(iteratorGrid === 1)
                     {
-                        gridRowFromItem2 = 4;
-                        gridRowToItem2 = 10;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowFromItem2 = 7;
+                            gridRowToItem2 = 12;
+                        }else{
+                            gridRowFromItem2 = 4;
+                            gridRowToItem2 = 10;
+                        }
+                        
                     }else{
                         gridRowFromItem2 = gridRowFromItem2 + mult;                    
-                        gridRowToItem2 = gridRowToItem2 + mult;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowToItem2 = gridRowFromItem2 + +5;
+                        }else{
+                            gridRowToItem2 = gridRowToItem2 + mult;
+                        }          
                     }
                     
                     
@@ -82,11 +135,23 @@ export default class GridProyects extends Component
                 case 'item-3':
                     if(iteratorGrid === 1)
                     {
-                        gridRowFromItem3 = 8;
-                        gridRowToItem3 = 14;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowFromItem3 =13;
+                            gridRowToItem3 = 18;
+                        }else{
+                            gridRowFromItem3 = 8;
+                            gridRowToItem3 = 14;
+                        }
+                        
                     }else{
                         gridRowFromItem3 = gridRowFromItem3 + mult;                    
-                        gridRowToItem3 = gridRowToItem3 + mult;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowToItem3 = gridRowFromItem3 + +5;
+                        }else{
+                            gridRowToItem3 = gridRowToItem3 + mult;
+                        }          
                     }
                     
                     return React.cloneElement(children, {style:{gridRow: `${gridRowFromItem3}/${gridRowToItem3}`}});
@@ -95,11 +160,23 @@ export default class GridProyects extends Component
                 case 'item-4':
                     if(iteratorGrid === 1)
                     {
-                        gridRowFromItem4 = 11;
-                        gridRowToItem4 = 17;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowFromItem4 = 19;
+                            gridRowToItem4 = 24;
+                        }else{
+                            gridRowFromItem4 = 11;
+                            gridRowToItem4 = 17;
+                        }
+                        
                     }else{
                         gridRowFromItem4 = gridRowFromItem4 + mult;                    
-                        gridRowToItem4 = gridRowToItem4 + mult;
+                        if(window.matchMedia("(max-width: 1200px)").matches)
+                        {
+                            gridRowToItem4 = gridRowFromItem4 + +5;
+                        }else{
+                            gridRowToItem4 = gridRowToItem4 + mult;
+                        }          
                     }
                     
                     
@@ -110,28 +187,30 @@ export default class GridProyects extends Component
         })
     }
 
-    addRows()
+    addRows(type)
     {
+        this.lengthRows = 0;
         this.props.children.forEach(children=>{
             switch(children.props.className)
             {
     
-                case 'item-1':                                       
-                    this.lengthRows = this.lengthRows + 7
+                case 'item-1':                 
+
+                    this.lengthRows = this.lengthRows +  ( type===0?7:6)
                     break;
                     
                 case 'item-2':
-                    this.lengthRows = this.lengthRows + 3
+                    this.lengthRows = this.lengthRows + ( type===0?3:6)
                     break;
                     
     
                 case 'item-3':
-                    this.lengthRows = this.lengthRows + 4
+                    this.lengthRows = this.lengthRows + ( type===0?4:6)
                     break;
                     
     
                 case 'item-4':
-                    this.lengthRows = this.lengthRows + 2
+                    this.lengthRows = this.lengthRows + ( type===0?2:6)
                     break;
                     
             }   
